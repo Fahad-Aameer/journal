705e1b53fd3062f5d521e2e0fc0cb285e27221e1
Fahad Aameer <fahadaameer@gmail.com>
2023-06-15 18:02:11 +0530

    Update

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/blogdb');
+  try {
+    await mongoose.connect('mongodb+srv://fahadaameer:Zaahil33@journal.r3lzk95.mongodb.net/journal?retryWrites=true&w=majority');
+    console.log("Success I suppose")
+  } catch (e) { 
+    console.log(e)
+  }
 }
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
